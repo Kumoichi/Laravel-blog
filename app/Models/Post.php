064eb18545f3fb5376894ac671bc980edeec5ab9
Kumoichi <minimi_ken1@yahoo.co.jp>
2023-07-18 18:01:45 +0900

    12-2 code

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
@@ -10,22 +11,34 @@ class Post
     public $excerpt;
     public $date;
     public $body;
+    public $slug;
 
     // Constructor
-    public function __construct($title, $excerpt, $date, $body)
+    public function __construct($title, $excerpt, $date, $body, $slug)
     {
         $this->title = $title;
         $this->excerpt = $excerpt;
         $this->date = $date;
         $this->body = $body;
+        $this->slug = $slug;
     }
 
 
 
     public static function all(){
-        $files =  File::files(resource_path("posts/"));
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+            $document->title,
+            $document->excerpt,
+            $document->date,
+            $document->body(),
+            $document->slug
+        ));
+
+        // $files =  File::files(resource_path("posts/"));
         
-        return array_map(fn($file) => $file->getContents(), $files); 
+        // return array_map(fn($file) => $file->getContents(), $files); 
     }
 
     public static function find($slug){
--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,8 @@ use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
 
 Route::get('/', function () {
     //$files contain all files information like file name,  inside the posts folder
-    $files =  File::files(resource_path("posts"));
-    $posts = []; 
-
-    foreach($files as $file){
-        //separates the contents of the "My_first_page.html"
-        //(and other similar files) into two parts: the YAML front matter and the body content.
-        //inside of $documents-> matter is something like this
-        //[
-        //     'title' => 'My Firstttt Post',
-        //     'excerpt' => 'Lorem ipsum dolor sit',
-        //     'date' => '2021-05-21'
-        // ]
-        $documents = YamlFrontMatter::parseFile($file);
-
-        $posts[] = new Post(
-            $documents->title,
-            $documents->excerpt,
-            $documents->date,
-            $documents->body()
-        );
-    }
-
-    return view('posts', ['posts' => $posts]);
+   
+    return view('posts', ['posts' => Post::all()]);
     // $post = Post::all();
     // return view ('posts', [
     //     'posts' => $post
