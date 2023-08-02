2682ea6049f843e5d0e5504ba0ecf296e2695a08
Kumoichi <minimi_ken1@yahoo.co.jp>
2023-08-02 18:05:16 +0900

    comments associated with a post 54

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    //I can use belongsTo User in Comment.php because  comments belongs to posts table 
+    //and posts belongs to users table
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
+
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,3 +1,5 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border-gray-200 p-6 rounded-xl space-x-4">
                         <div class="flex-shrink-0">
                             <img src="https://i.pravator.cc/100" alt="this is image" width="60" height="60" class="rounded-xl">
@@ -5,10 +7,10 @@
 
                         <div>
                             <header class="mb-4">
-                                <strong class="font-bold">John Doe</strong>
+                                <strong class="font-bold">{{ $comment->author->username }}</strong>
                                 <p class="text-xs">Posted
-                                    <time>8 month ago</time></p>
+                                    <time>{{ $comment->created_at }}</time></p>
                             </header>
-                            <p>This is the content. This is the content. This is the content. This is the content.</p>
-                        </div>
+                        <p>{{ $comment->body }}</p>                       
+                     </div>
                     </article>
