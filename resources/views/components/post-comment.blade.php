0af63eb8aee994e2a58928b236c97b1979461bcb
Kumoichi <minimi_ken1@yahoo.co.jp>
2023-08-02 14:58:18 +0900

    comment frontend 51

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,14 @@
+<article class="flex bg-gray-100 border-gray-200 p-6 rounded-xl space-x-4">
+                        <div class="flex-shrink-0">
+                            <img src="https://i.pravator.cc/100" alt="this is image" width="60" height="60" class="rounded-xl">
+                        </div>
+
+                        <div>
+                            <header class="mb-4">
+                                <strong class="font-bold">John Doe</strong>
+                                <p class="text-xs">Posted
+                                    <time>8 month ago</time></p>
+                            </header>
+                            <p>This is the content. This is the content. This is the content. This is the content.</p>
+                        </div>
+                    </article>
